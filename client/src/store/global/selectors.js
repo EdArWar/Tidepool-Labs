7702a637a1425f4d892261a04fe741897b693054
EdArWar <edgar.vard92@gmail.com>
2022-02-20 16:15:11 +0400

    removing unwanted selectors

--- client/src/store/global/selectors.js
+++ client/src/store/global/selectors.js
@@ -2,22 +2,8 @@ import { createSelector } from "reselect";
 
 const globalSelector = (state) => state.global;
 
-const isAuth = createSelector([globalSelector], (global) => global.isAuth);
-
 const loader = createSelector([globalSelector], (global) => global.loader);
 
-const token = createSelector([globalSelector], (global) => global.token);
-
-const isRegistered = createSelector(
-  [globalSelector],
-  (global) => global.isRegistered
-);
-
-const updatePost = createSelector(
-  [globalSelector],
-  (global) => global.updatePost
-);
-
-const selectors = { isAuth, loader, token, isRegistered, updatePost };
+const selectors = { loader };
 
 export default selectors;
